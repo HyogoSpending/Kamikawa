7eee384607654e006f1b4913499b659005631635
HyogoSpending <HyogoSpending@gmail.com>
2014-06-09 17:20:34 +0900

    up

---
+++ js/setting.js
@@ -0,0 +1,68 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'kamikawa_2012_kessan';//平成24年度神河町決算 
+OpenSpending.year = '2012';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* kamikawa cofog */
+  '20120100': { icon: 'icons/legislative.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120101': { icon: 'icons/legislative.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120200': { icon: 'icons/publicaffairs.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120201': { icon: 'icons/admin.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120202': { icon: 'icons/japanese-money.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120203': { icon: 'icons/publicaffairs.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120204': { icon: 'icons/legislative.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120205': { icon: 'icons/research.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120206': { icon: 'icons/publicaffairs.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120300': { icon: 'icons/helping-others.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120301': { icon: 'icons/helping-others.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120302': { icon: 'icons/schools.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120400': { icon: 'icons/health.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120401': { icon: 'icons/health.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120402': { icon: 'icons/environment.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120403': { icon: 'icons/garbage.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120500': { icon: 'icons/farms.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120501': { icon: 'icons/farms.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120502': { icon: 'icons/forest.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120503': { icon: 'icons/harbor.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120600': { icon: 'icons/economy-tourism.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120601': { icon: 'icons/economy-tourism.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120700': { icon: 'icons/construction.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120701': { icon: 'icons/our-streets.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120702': { icon: 'icons/our-streets.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120703': { icon: 'icons/construction1.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120704': { icon: 'icons/planning.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120705': { icon: 'icons/housing.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120800': { icon: 'icons/fire-brigade.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120801': { icon: 'icons/fire-brigade.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120900': { icon: 'icons/education.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120901': { icon: 'icons/publicaffairs.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120902': { icon: 'icons/secondary-lower.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120903': { icon: 'icons/primary.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120904': { icon: 'icons/pre-school.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120905': { icon: 'icons/civilian-action.svg', color: '#F58181', bcolor: '#F58181' },
+  '20120906': { icon: 'icons/sports.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121000': { icon: 'icons/public-debt.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121001': { icon: 'icons/public-debt.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121100': { icon: 'icons/money.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121101': { icon: 'icons/money.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121102': { icon: 'icons/money.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121200': { icon: 'icons/aid-developing-countries.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121201': { icon: 'icons/aid-developing-countries.svg', color: '#F58181', bcolor: '#F58181' },
+  '20121202': { icon: 'icons/aid-developing-countries.svg', color: '#F58181', bcolor: '#F58181' },
+　'20121203': { icon: 'icons/aid-developing-countries.svg', color: '#F58181', bcolor: '#F58181' }
+};
+
